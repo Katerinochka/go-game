8769df783d4cad90f5ef2f53743af50c8f750a86
BukhalovaK <bukhalova-em@yandex.ru>
2023-03-25 16:43:06 +0300

    add weight for user and things

--- main.go
+++ main.go
@@ -17,18 +17,19 @@ type Room struct {
 type Human struct {
 	Location Room
 	PutOn    []Cloth
+	Weight   int
+	Take     []Thing
 }
 
 type Cloth struct {
-	Name     string
-	Capacity int
-	Where    string
-	Things   map[string]Thing
+	Name   string
+	Weight int
 }
 
 type Thing struct {
-	Name  string
-	Where string
+	Name   string
+	Where  string
+	Weight int
 }
 
 type Rooms map[string]*Room
@@ -53,18 +54,21 @@ func NewRoom(name string, entrance []string) *Room {
 func NewUser(room Room) *Human {
 	u := new(Human)
 	u.Location = room
+	u.Weight = 0
 	return u
 }
 
-func NewCloth(name string) *Cloth {
+func NewCloth(name string, weight int) *Cloth {
 	c := new(Cloth)
 	c.Name = name
+	c.Weight = weight
 	return c
 }
 
-func NewThing(name string) *Thing {
+func NewThing(name string, weight int) *Thing {
 	t := new(Thing)
 	t.Name = name
+	t.Weight = weight
 	return t
 }
 
@@ -87,9 +91,9 @@ func (h Human) PrintCanGo() {
 	fmt.Println("можно пойти в", strings.Join(h.Location.Entrance, ", "))
 }
 
-func (r *Room) AddClothes(clothes ...string) {
-	for _, cloth := range clothes {
-		r.Clothes[cloth] = *NewCloth(cloth)
+func (r *Room) AddClothes(clothes map[string]int) {
+	for cloth, weight := range clothes {
+		r.Clothes[cloth] = *NewCloth(cloth, weight)
 	}
 }
 
@@ -108,20 +112,31 @@ func (h Human) LookAround() {
 func (h *Human) ToPutOn(cloth string) bool {
 	if v, ok := h.Location.Clothes[cloth]; ok {
 		h.PutOn = append(h.PutOn, v)
+		h.Weight += v.Weight
 		delete(h.Location.Clothes, cloth)
 		return true
 	}
 	return false
 }
 
-func (r *Room) AddThing(things ...string) {
-	for _, thing := range things {
-		r.Things[thing] = *NewThing(thing)
+func (r *Room) AddThing(things map[string]int) {
+	for thing, weight := range things {
+		r.Things[thing] = *NewThing(thing, weight)
 	}
 }
 
-func (h *Human) Take(thing string) {
-
+func (h *Human) ToTake(thing string) bool {
+	if v, ok := h.Location.Things[thing]; ok {
+		if h.Weight < v.Weight {
+			fmt.Println("некуда класть")
+			return false
+		}
+		h.Take = append(h.Take, v)
+		h.Weight -= v.Weight
+		delete(h.Location.Things, thing)
+		return true
+	}
+	return false
 }
 
 func init() {
@@ -129,9 +144,12 @@ func init() {
 	Locations.AddRoom("кухня", "коридор")
 	Locations.AddRoom("коридор", "кухня", "комната", "улица")
 	Locations.AddRoom("комната", "коридор")
-	Locations["комната"].AddClothes("рюкзак")
-	Locations["комната"].AddThing("конспекты")
-	fmt.Println(Locations["комната"])
+	Locations["комната"].AddClothes(map[string]int{
+		"рюкзак": 10,
+	})
+	Locations["комната"].AddThing(map[string]int{
+		"конспекты": 1,
+	})
 	Locations.AddRoom("улица", "коридор")
 	User = *NewUser(*Locations["кухня"])
 }
@@ -155,6 +173,13 @@ func main() {
 			fmt.Scan(&parametr)
 			if User.ToPutOn(parametr) {
 				fmt.Println("Вы надели:", parametr)
+			} else {
+				fmt.Println("нет такого")
+			}
+		case "взять":
+			fmt.Scan(&parametr)
+			if User.ToTake(parametr) {
+				fmt.Println("Вы взяли:", parametr)
 			} else {
 				fmt.Println("нет такого")
 			}
